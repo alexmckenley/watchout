43bfacb096b218398e70957fd7bd136db6663de6
HackReactor <caasjj@gmail.com>
2014-01-17 15:29:53 -0800

    moving enemies every second using a transition

--- script.js
+++ script.js
@@ -6,5 +6,14 @@ var arena = d3.select('body').append('svg').style('width', width).style('height'
 arena.selectAll('circle').data(d3.range(40)).enter().append('circle').attr({
   'r': 20,
   'cx': function(){return Math.random() * width;},
-  'cy': function(){return Math.random() * height;},
+  'cy': function(){return Math.random() * height;}
 });
+
+var move = function() {
+  arena.selectAll('circle').transition().duration(2000).attr({
+    'cx': function(){return Math.random() * width;},
+    'cy': function(){return Math.random() * height;}
+  });
+};
+
+setInterval(move, 2000);
