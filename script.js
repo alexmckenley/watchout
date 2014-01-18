62712f086cf39d94acfecc85df862b323c7d8683
HackReactor <caasjj@gmail.com>
2014-01-17 19:05:57 -0800

    implemented collision detection

--- script.js
+++ script.js
@@ -1,25 +1,39 @@
 var width = window.innerWidth;
 var height = window.innerHeight;
+var radius = 20;
 
 
 var arena = d3.select('body').append('svg').style('width', width).style('height', height);
 
 //enemies
-arena.selectAll('circle').data(d3.range(40)).enter().append('circle').attr({
-  'r': 20,
+arena.selectAll('circle').data(d3.range(20)).enter().append('circle').attr({
+  'r': radius,
   'cx': function(){return Math.random() * width;},
   'cy': function(){return Math.random() * height;},
   'class': 'enemy'
 });
 
 var move = function() {
-  arena.selectAll('.enemy').transition().duration(2000).attr({
+  arena.selectAll('.enemy')
+  .transition()
+  .duration(2000)
+  .attr({
     'cx': function(){return Math.random() * width;},
     'cy': function(){return Math.random() * height;}
-  });
+  })
+  .tween('custom', function(d, i, a){
+    var enemy = d3.select(this);
+
+    return function(){
+      checkCollision(enemy, i);
+
+    }
+  }).ease();
 };
 
-setInterval(move, 2000);
+move();
+
+setInterval(move, 1600);
 
 //player
 var drag = d3.behavior.drag()
@@ -30,11 +44,30 @@ var drag = d3.behavior.drag()
       });
   });
 
-arena.append('circle').attr({
+var player = arena.append('circle').attr({
   'fill': 'orange',
-  'r': 20,
+  'r': radius,
   'cx': function(){return width/2;},
   'cy': function(){return height/2;},
   'class': 'player'
-}).call(drag);
+})
+
+player.call(drag);
+
+
+
+//collision detection
+var collisions = 0;
+
+var checkCollision = function(enemy, i){
+  var r = radius;
+  var x = enemy.attr('cx') - player.attr('cx');
+  var y = enemy.attr('cy') - player.attr('cy');
+  var dist = Math.sqrt(x * x + y * y);
+  if(dist < 2 * radius) {
+    collisions += 1;
+    //debugger;
+    console.log('Collision with ', i);
+  }
+};
 
